dc8afadf69c3ce703e660b5ac2719362227282a4
MervyCrisskays01 <mervydieubeni0@gmail.com>
2023-05-01 01:33:07 +0200

    js modifier

--- js.js
+++ js.js
@@ -1,2 +1,9 @@
-let hello;
-const annita = 2;
+let identite = {
+  nom: "ETSORA",
+  prenom: "Dieu-beni",
+  age: 25,
+};
+
+console.log(
+  `je m'appel ${identite.nom}, mon prenom est : ${identite.prenom} et j'ai ${identite.age} ans`
+);
